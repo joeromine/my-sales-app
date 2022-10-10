5a810e0635f113aa1681fc3dfece341e1f3d8431
Joe Romine <joe.romine@ufl.edu>
2022-10-10 14:58:53 -0700

    added supplier svc

---
+++ src/app/suppliers/supplier.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Supplier } from './supplier.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+
+  constructor(private http: HttpClient) { }
+
+
+  public getAll(): Observable<Supplier[]>{
+    return this.http.get<Supplier[]>(environment.api + 'suppliers')
+  }
+
+  public getById(id: Number): Observable<Supplier> {
+     return this.http.get<Supplier>(environment.api + 'suppliers/' + id);
+  }
+
+  public save(supplier: Supplier): Observable<Supplier> {
+     if (supplier.id){
+        return this.http.put<Supplier>(environment.api + 'suppliers/' + supplier.id, supplier);
+      }
+     return this.http.post<Supplier>(environment.api + 'suppliers', supplier);
+     }
+
+     public delete(id?: number): Observable<Supplier> {
+       return this.http.delete<Supplier>(environment.api + 'suppliers/' + id);
+      }
+
+      public create(): Observable<Supplier> {
+         return of<Supplier>(
+          {
+         id: 0,
+         companyName: '',
+         contactName: '',
+         contactTitle: '',
+         address: {
+          city: '',
+          phone: '',
+          country: '',
+          postalCode: 0,
+          region: '',
+          street: ''
+         }
+        }
+        );
+      }
+
+}
