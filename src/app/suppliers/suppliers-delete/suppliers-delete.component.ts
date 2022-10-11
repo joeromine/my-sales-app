e43cc69d11c0fc4f90266c635724b27810efded3
Joe Romine <joe.romine@ufl.edu>
2022-10-10 18:56:14 -0700

    add delete

--- src/app/suppliers/suppliers-delete/suppliers-delete.component.ts
+++ src/app/suppliers/suppliers-delete/suppliers-delete.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { lastValueFrom, Observable } from 'rxjs';
+import { Supplier } from '../supplier.dto';
+import { SupplierService } from '../supplier.service';
 
 @Component({
   selector: 'app-suppliers-delete',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppliersDeleteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private supplierService: SupplierService,
+    private route: ActivatedRoute,
+     private router: Router) { }
 
-  ngOnInit(): void {
+      supplier!: Supplier;
+     supplierObservable!: Observable<Supplier>;
+
+  async ngOnInit(){
+    const id: Number = +(this.route.snapshot.paramMap.get('id') || 0)
+    this.supplierObservable = this.supplierService.getById(id);
+     this.supplier = await lastValueFrom(this.supplierObservable);
+
+  }
+
+
+  async confirmDelete(){
+    this.supplierObservable = this.supplierService.delete(this.supplier.id)
+     await lastValueFrom(this.supplierObservable)
+    this.router.navigate(['/suppliers']);
   }
 
 }
--- src/app/suppliers/suppliers-new/suppliers-new.component.ts
+++ src/app/suppliers/suppliers-new/suppliers-new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { lastValueFrom, Observable } from 'rxjs';
+import { Supplier } from '../supplier.dto';
+import { SupplierService } from '../supplier.service';
 
 @Component({
   selector: 'app-suppliers-new',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppliersNewComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private supplierService: SupplierService,
+              private router: Router) { }
+  supplierObservable!: Observable<Supplier>;
+  supplier!: Supplier
   ngOnInit(): void {
+    this.supplierObservable = this.supplierService.create();
+    this.supplierObservable.subscribe( supplier => {
+    this.supplier = supplier;
+ })
   }
+
+
+  async onSave(supplier: Supplier) {
+     this.supplierObservable = this.supplierService.save(supplier)
+     const result = await lastValueFrom(this.supplierObservable);
+     this.router.navigate(['/suppliers/show', result.id]);
+    }
 
 }
